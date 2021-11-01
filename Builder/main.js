305c8373ff4cf8a61fc9e7aec2c7b438a96fc89a
rhyan eduardo <rhyaneduardo91@gmail.com>
2021-11-01 17:23:06 -0300

    01.11.21 17.22

--- Builder/main.js
+++ Builder/main.js
@@ -3,18 +3,18 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Cria uma janela de navegação.
   let win = new BrowserWindow({
-    width: 930,
-    height: 600,
-    resizable: false,
+    width: 1366,
+    height: 768,
+    resizable: true,
     frame: true,
-    icon: __dirname + "/fly.png",
+    icon: __dirname + "/icon.png",
     //minWidth: 1366,
     //minHeight: 700,
   });
 
   // e carregar o index.html do aplicativo.
   win.removeMenu();
-  win.loadFile('index.html')
+  win.loadFile('Screens/Welcome/WelcomeWindow.html')
 }
 
 app.whenReady().then(createWindow)
